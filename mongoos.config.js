d6eb2440527fbc9621c48caeee0985a738549fb9
harendrasinhp <haren111990@gmail.com>
2025-01-12 20:42:02 +0530

    mongoos connection changess for deprecated.

--- mongoos.config.js
+++ mongoos.config.js
@@ -4,12 +4,7 @@ const url = "mongodb://localhost:27017/chatterUp"
 
 export const ConnectingToDbBymongoos = async () => {
    try{
-    await mongoose.connect(url,
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        }
-    )
+    await mongoose.connect(url);
     console.log("Connected to MongoDb...")
    }catch(err){
     console.log(err)
